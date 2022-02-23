6238d74c72839d1b187701691db2df6a8320264d
villadsfrede <74766251+villadsfrede@users.noreply.github.com>
2022-02-23 12:31:50 +0100

    Delete kommit2 directory

--- kommit2/forside.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", function(e) {
-    if(e.keyCode ==37) {
-        alert("left was pressed")
-    }
-    if(e.keyCode ==39) {
-        window.location.href = "info/info.html"
-    }
-})
